ae0d4e37745959792d6518e793b34f25a607ecf2
whyjava7 <whyjava7@a3947ebe-0202-11df-8f7b-bfe16de6f99b>
2010-01-15 20:54:33 +0000

    committing solution to problem 7 of project euler. Added another method in ProjectEulerUtils to help solve problem 7.

---
+++ src/org/project/euler/PE7.java
@@ -0,0 +1,28 @@
+package org.project.euler;
+
+
+/**
+ * 	By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13,
+ *  we can see that the 6th prime is 13.
+ *  What is the 10001th prime number?
+ *  
+ *  <a href="http://projecteuler.net/index.php?section=problems&id=7">Problem 7</a>.
+ * 
+ * @author Shekhar
+ *
+ */
+public class PE7 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		StopWatch watch = new StopWatch().start();
+		int nthPrimeNumber = ProjectEulerUtils.findNthPrimeNumber(10001);
+		System.out.println(nthPrimeNumber);
+		watch.stop().getTimeElapsed();
+	}
+	
+	
+	
+}
