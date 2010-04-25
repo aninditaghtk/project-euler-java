775a034bef359104b0ded753b153a4c1fa5ea618
whyjava7 <whyjava7@a3947ebe-0202-11df-8f7b-bfe16de6f99b>
2010-04-25 20:34:41 +0000

    After a long time, i am starting again. Committing solution and junit testcase of first problem

---
+++ testsrc/org/project/euler/PE1Test.java
@@ -0,0 +1,43 @@
+package org.project.euler;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PE1Test {
+	
+	private PE1 problem;
+
+	@Before
+	public void setUp() throws Exception {
+		problem = new PE1();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		problem = null;
+	}
+	
+	@Test
+	public void test_sum_of_multiples_of_3_and_5_below_10(){
+		int firstNumber = 3;
+		int secondNumber = 5;
+		int limit = 10;
+		
+		int sum = problem.calculateSum(limit,firstNumber,secondNumber);
+		assertEquals(23, sum);
+	}
+	
+	@Test
+	public void test_sum_of_multiples_of_3_and_5_below_1000(){
+		int firstNumber = 3;
+		int secondNumber = 5;
+		int limit = 1000;
+		int sum = problem.calculateSum(limit,firstNumber,secondNumber);
+		assertEquals(233168, sum);
+	}
+
+}
